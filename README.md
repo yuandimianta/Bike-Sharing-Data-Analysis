a475c6f9515cd39ef2a464c04f2ac14555a23440
yuandimianta <101907492+yuandimianta@users.noreply.github.com>
2024-03-05 15:28:07 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Bike-Sharing-Data-Analysis
+This project covered from defining the business questions based on the given data until visualizing it to solve the problems.
+
+## Setup environment
+```
+conda create --name main-ds python=3.12.2
+conda activate main-ds
+pip install numpy==1.24.3 pandas==2.0.3 matplotlib==3.7.2 seaborn==0.13.2 streamlit==1.31.1 babel==2.11.0
+```
+
+## Run steamlit app
+```
+streamlit run Dashboard.py
+```
---
+++ dashboard/Dashboard.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load data
+hour_df = pd.read_csv("hour.csv")
+
+# Set page title
+st.title("Bike Sharing Dashboard")
+
+# Sidebar
+st.sidebar.title("Menu")
+selected_chart = st.sidebar.selectbox("Pilih Grafik", ["Pengaruh Musim", "Pengaruh Hari Kerja", "Pengaruh Hari Libur"])
+
+# Fungsi untuk menggambar grafik
+def draw_chart(data, x_label, y_label, title):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    sns.barplot(x=data['x'], y=data['y'], palette=data['color'], ax=ax)
+    ax.set_title(title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_xticklabels(data['labels'], rotation=45)
+    st.pyplot(fig)
+
+# Draw selected chart based on sidebar choice
+if selected_chart == "Pengaruh Musim":
+    seasonal_counts = hour_df.groupby('season')['cnt'].mean().reset_index()
+    chart_data = {'x': seasonal_counts['season'], 'y': seasonal_counts['cnt'], 'color': sns.color_palette("pastel"), 'labels': ['Musim ' + str(int(x)) for x in seasonal_counts['season']]}
+    draw_chart(chart_data, 'Musim', 'Rata-rata Jumlah Sewa Sepeda', 'Pengaruh Musim pada Jumlah Sewa Sepeda')
+
+elif selected_chart == "Pengaruh Hari Kerja":
+    workingday_counts = hour_df.groupby('workingday')['cnt'].mean().reset_index()
+    chart_data = {'x': workingday_counts['workingday'], 'y': workingday_counts['cnt'], 'color': sns.color_palette("pastel"), 'labels': ['Bukan Hari Kerja', 'Hari Kerja']}
+    draw_chart(chart_data, 'Hari Kerja', 'Rata-rata Jumlah Sewa Sepeda', 'Pengaruh Hari Kerja terhadap Jumlah Sewa Sepeda')
+
+elif selected_chart == "Pengaruh Hari Libur":
+    holiday_counts = hour_df.groupby('holiday')['cnt'].mean().reset_index()
+    chart_data = {'x': holiday_counts['holiday'], 'y': holiday_counts['cnt'], 'color': sns.color_palette("pastel"), 'labels': ['Bukan Hari Libur', 'Hari Libur']}
+    draw_chart(chart_data, 'Hari Libur', 'Rata-rata Jumlah Sewa Sepeda', 'Pengaruh Hari Libur terhadap Jumlah Sewa Sepeda')
